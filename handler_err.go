60b287019f657f7fc08cebf6fd5c93268b8cabda
Samuel Adeniyi <sadeniyi@afexnigeria.com>
2023-07-13 14:52:31 +0100

    fixed all bugs

--- handler_err.go
+++ handler_err.go
@@ -3,5 +3,5 @@ package main
 import "net/http"
 
 func handlerErr(w http.ResponseWriter, r *http.Request) {
-	responseWithError(w, 200, "Something went wrong")
+	respondWithError(w, 200, "Something went wrong")
 }
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
@@ -9,7 +10,14 @@ import (
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
+
+	"github.com/aisamuel/rssagg/internal/database"
+	_ "github.com/lib/pq" // <------------ here
 )
+
+type apiConfig struct {
+	DB *database.Queries
+}
 
 func main() {
 	fmt.Println("hello world")
@@ -19,6 +27,26 @@ func main() {
 	portString := os.Getenv("PORT")
 	if portString == "" {
 		log.Fatal("PORT is not found in the environment")
+	}
+
+	dbURL := os.Getenv("DB_URL")
+	if dbURL == "" {
+		log.Fatal("DB_URL is not found in the environment")
+	}
+
+	conn, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		fmt.Println("Couldn't create user:", err)
+		log.Fatal("Can't connect to database")
+	}
+
+	// queries, err := database.New(conn)
+	// if err != nil {
+	// 	log.Fatal("Can't connect to db connection", err)
+	// }
+
+	apiCfg := apiConfig{
+		DB: database.New(conn),
 	}
 
 	router := chi.NewRouter()
@@ -36,6 +64,7 @@ func main() {
 
 	v1Router.Get("/healthz", handlerReadiness)
 	v1Router.Get("/err", handlerErr)
+	v1Router.Post("/users", apiCfg.handlerCreateUser)
 
 	router.Mount("/v1", v1Router)
 
@@ -45,7 +74,7 @@ func main() {
 	}
 
 	log.Printf("Server starting on port %v", portString)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	if err != nil {
 		log.Fatal(err)
 	}
